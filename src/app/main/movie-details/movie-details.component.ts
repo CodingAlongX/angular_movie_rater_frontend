343d7eb3a7e4d8464058bc924b647ee94e67657b
S. M. JAHANGIR <smjxpro@gmail.com>
2020-07-26 18:23:13 +0600

    movie details

--- src/app/main/movie-details/movie-details.component.ts
+++ src/app/main/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  movie;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
